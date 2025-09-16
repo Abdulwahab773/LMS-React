6b65114d4872044c6c57aa078c2569294090f03b
Abdulwahab773 <abdulwahabyou@gmail.com>
2025-09-16 16:33:47 +0500

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfDrWEhV1pCo6M491alKT6lxg4rmzIGT0",
+  authDomain: "lms-react-59538.firebaseapp.com",
+  projectId: "lms-react-59538",
+  storageBucket: "lms-react-59538.firebasestorage.app",
+  messagingSenderId: "584806137864",
+  appId: "1:584806137864:web:63091d0e6c49d71e6911bd"
+};
+
+const app = initializeApp(firebaseConfig);
