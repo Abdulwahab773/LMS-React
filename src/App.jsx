21a9e6ed335e8bc851828d2857b734cb9014d8c9
Abdulwahab773 <abdulwahabyou@gmail.com>
2025-09-18 20:14:28 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,33 @@
-import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
 
-import LoginPage from './pages/login'
-import AuthRoute from './Routes/AuthRoute'
-import SingUpPage from './pages/singup'
-import AdminDeshbord from './pages/Admin/AdminDeshbord'
-import PrivateRoute from './Routes/PrivateRoute'
-import NotFound from './pages/NotFound/NotFound'
-import UserCourse from './pages/UserCourse'
-import AllCourses from './pages/UserCourse/AllCourses'
-import UserCourses from './pages/UserDeshbord/UserCourses'
-import UserDeshbord from './pages/UserDeshbord'
-
+import LoginPage from "./pages/login";
+import AuthRoute from "./Routes/AuthRoute";
+import SingUpPage from "./pages/singup";
+// import AdminDeshbord from './pages/Admin/AdminDeshbord'
+import PrivateRoute from "./Routes/PrivateRoute";
+import NotFound from "./pages/NotFound/NotFound";
+import UserCourse from "./pages/UserCourse";
+import AllCourses from "./pages/UserCourse/AllCourses";
+import UserCourses from "./pages/UserDeshbord/UserCourses";
+import UserDeshbord from "./pages/UserDeshbord";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route element={<AuthRoute />}>
-          <Route index element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/Signup" element={<SingUpPage />} />
         </Route>
 
-<Routes>
-
-<Route  element={<AuthRoute />}>
-  
-  <Route  path="/login" element={<LoginPage />}  />
-  <Route  path="/Signup" element={<SingUpPage  />}  />
-
-</Route>
-
-<Route  path="*" element={<NotFound  />}  />
-<Route  index element={<UserCourse  />}  />
-<Route  path='/All-Courses' element={<AllCourses />} />
-
-
-
-
-<Route element={<PrivateRoute />} >
-
-
-<Route  path='/UserDeshbord'  element={<UserDeshbord /> }/>
-<Route path='/userCourses' element={<UserCourses />} />
-
-
-</Route>
-
-
-
-
-</Routes>
-
+        <Route path="*" element={<NotFound />} />
+        <Route index element={<UserCourse />} />
+        <Route path="/All-Courses" element={<AllCourses />} />
 
         <Route element={<PrivateRoute />}>
           <Route path="/UserDeshbord" element={<UserDeshbord />} />
+          <Route path="/userCourses" element={<UserCourses />} />
         </Route>
       </Routes>
     </>
